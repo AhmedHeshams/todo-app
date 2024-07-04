37a6b5197a4b36b30199c5bfcbb9ea59aed41a08
Ahmed Hesham <ahmed.hesham_1999@yahoo.com>
2024-07-04 07:36:11 +0300

    Adding mongodb to the project to store todo app data instead of an array

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,26 +1,54 @@
 var bodyParser = require('body-parser');
+var mongoose   = require("mongoose"); 
+var dotenv     = require("dotenv");
+dotenv.config();
+mongoose.connect(process.env.URI);
 
+// creat a schema
+todoSchema = new mongoose.Schema({
+    item: String
+})
 
-// the data
-data=[{item:"clean"},{item:"study"},{item:"eat"}];
+// creat a model
+Todo = mongoose.model("Todo", todoSchema);
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 module.exports = function(app){
 app.get("/todo",function(req,res){
-    res.render("to-do",{todo:data});
+    Todo.find({})
+    .then(function(data){
+        res.render("to-do",{todo:data});
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 });
 
 app.post("/todo",urlencodedParser,function(req,res){
-    data.push(req.body);
-    res.json(data);
+    // get the data from the view and add it to mongodb
+    var newTodo = Todo(req.body).save()
+    .then(function(data){
+        console.log("item saved");
+        res.json(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+    
 });
 app.delete("/todo/:item",function(req,res){
-    data = data.filter(function(todo){
-        // return todo.item.replace(/ /g, "-") !== req.params.item;
-        return todo.item !== req.params.item.trim();
+    console.log(req.params.item.trim());
+    deletedTodo = Todo.find({item:req.params.item.trim()}).deleteOne()
+    .then(function(data){
+        console.log("item deleted");
+        res.json(data);
     })
-    res.json(data);
+    .catch(function (err) {
+        console.log(err);
+    });
+    
+    
 });
 };
